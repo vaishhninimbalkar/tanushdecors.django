429b0538ab8742ccf1d56ea568648ab1499d9ae5
Ralph Göstenmeier <r14r@via-internet.de>
2024-12-26 11:55:19 +0100

    -

--- project/settings.py
+++ project/settings.py
@@ -146,4 +146,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'tanushdecor@gmail.com'
-EMAIL_HOST_PASSWORD = 'jtud hism ccvp gien'
+EMAIL_HOST_PASSWORD = 'j'
