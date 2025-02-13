caa1cb1d7799de8da2377eda8e0cbf4cb6353223
Ralph Göstenmeier <r14r@via-internet.de>
2025-02-13 09:48:34 +0100

    -

--- .set_env.sh
+++ .set_env.sh
@@ -1,8 +1,8 @@
-export ENV_NAME=Coding4Beginner-Vaishnavi
+export ENV_NAME=Default
 export ENV_HOME=$PWD
 export     HOME=$ENV_HOME
 
 . venv env        init
 . venv vscode     init
 
-. venv python     init 3.12
+. venv python     init 3.11
--- tanushdecors_order.sql
+++
@@ -1,4 +0,0 @@
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "tanushdecors_order" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "customer_first_name" varchar(100) NOT NULL, "customer_last_name" varchar(100) NOT NULL, "customer_email" varchar(254) NOT NULL, "customer_phone" varchar(20) NOT NULL, "customer_address" varchar(255) NOT NULL, "customer_state_country" varchar(100) NOT NULL, "total_amount" decimal NOT NULL, "created_at" datetime NOT NULL);
-COMMIT;
